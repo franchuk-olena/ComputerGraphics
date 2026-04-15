10b5e4845862f27d5f9d49e02cea123e35d24204
 Olena <olena@Users-MacBook-Pro.local>
2026-04-15 18:05:49 -0500

    less02

---
+++ OpenGL_Start/src/lesson02/shader_utils.h
@@ -0,0 +1,16 @@
+
+
+#ifndef OPENGL_START_SHADER_UTILS_H
+#define OPENGL_START_SHADER_UTILS_H
+#include <string>
+
+#include "glad/glad.h"
+
+
+std::string LoadShaderFromFile(const std::string &filePath);
+GLuint  createShader(std::string &filePath, GLuint shaderType);
+GLuint createProgram(
+    std::string &vertexShaderName,
+    std::string &fragmentShaderName);
+
+#endif //OPENGL_START_SHADER_UTILS_H
